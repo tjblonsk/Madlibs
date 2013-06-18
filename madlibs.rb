f8fd2e5f7516fb29c8d77bfa59c9b51de7a27d6f
Tim <tim.blonski@gmail.com>
2013-06-18 12:17:30 -0400

    first commit

---
+++ madlibs.rb
@@ -0,0 +1,25 @@
+
+puts "Name the person!"
+Person_name = gets.chomp
+puts "Something the person does!"
+Action1 = gets.chomp
+puts "something else the person does"
+Action2 = gets.chomp
+puts "any song you like!"
+Song_name = gets.chomp
+puts "any language you like!"
+Language = gets.chomp
+puts "a store preferably!"
+Place = gets.chomp
+puts "something you can get at a store"
+Noun = gets.chomp
+puts "any weekday"
+Day_of_the_week = gets.chomp
+puts "either Sat or Sunday"
+Saturday_or_Sunday = gets.chomp
+puts "how he feels!"
+Adjective = gets.chomp
+
+puts "#{Person_name} went for a #{Action1}. Then he took at #{Action2}. After that, #{Person_name} sang #{Song_name} in #{Language}.
+Then he went to the #{Place}. At the #{Place} he bought a #{Noun}. This was a busy #{Day_of_the_week}. #{Person_name} loves
+the weekend, his favorite day is #{Saturday_or_Sunday} because it makes him feel #{Adjective}."
